bca0f547504d7d5175784a28ae14739e7409f61b
gomwing <gomwing@yahoo.com>
2024-03-23 11:43:59 +0900

    out.cpp 끝냈다. reg.cpp만 하면 된다..

--- out.cpp
+++ out.cpp
@@ -10,23 +10,24 @@
 
 #include "spc700.hpp"
 
-class spc700 : public outctx_t
+class out_spc700_t : public outctx_t
 {
-	void outreg(int r) { out_register(ph.reg_names[r]); }
-	void outmem(const op_t& x, ea_t ea);
-	bool outbit(ea_t ea, int bit);
-
+	out_spc700_t(void) : outctx_t(BADADDR) {} // not used
+private:
+	void out_dp(const op_t& x);
+public:
 	bool out_operand(const op_t& x);
 	void out_insn(void);
-	void out_mnem(void);
-}
+};
+CASSERT(sizeof(out_spc700_t) == sizeof(outctx_t));
 
-DECLARE_OUT_FUNCS(spc700)
+DECLARE_OUT_FUNCS_WITHOUT_OUTMNEM(out_spc700_t)
 
  //----------------------------------------------------------------------
-static void out_dp(op_t& x)
+//static void out_spc700_t::out_dp(op_t& x)
+void out_spc700_t::out_dp(const op_t& x)
 {
-	sel_t dp = get_sreg(cmd.ea, rFp);
+	sel_t dp = get_sreg(insn.ea, rFp);
 	if (dp != BADSEL)
 	{
 		ea_t ea = (dp << 8) | x.addr;
@@ -37,9 +38,10 @@ static void out_dp(op_t& x)
 		if (!out_name_expr(x, ea, BADADDR))
 		{
 			out_tagon(COLOR_ERROR);
-			OutLong(ea, 16);
+			out_btoa /*		OutLong*/(ea, 16);
 			out_tagoff(COLOR_ERROR);
-			QueueSet(Q_noName, cmd.ea);
+			//QueueSet(Q_noName, cmd.ea);
+			remember_problem(PR_NONAME, insn.ea);
 		}
 
 		//if ( dp != 0 )
@@ -53,37 +55,39 @@ static void out_dp(op_t& x)
 	}
 	else
 	{
-		OutValue(x, OOF_ADDR | OOFS_NOSIGN | OOFW_8);
+		out_value /*OutValue*/(x, OOF_ADDR | OOFS_NOSIGN | OOFW_8);
 	}
 }
 
 //----------------------------------------------------------------------
-bool idaapi outop(op_t& x)
+//bool idaapi outop(op_t& x)
+bool out_spc700_t::out_operand(const op_t& x)
 {
 	ea_t ea;
 	switch (x.type)
 	{
 	case o_reg:
-		out_register(ph.regNames[x.reg]);
+		out_register(ph.reg_names[x.reg]);
 		break;
 	case o_imm:
 		out_symbol('#');
-		OutValue(x, 0);
+		out_value(x, 0);
 		break;
 	case o_near:
 	case o_far:
 	case o_mem:
-		if (cmd.indirect)
+		if (insn.indirect)
 			out_symbol('(');
 		ea = x.addr;
 		if (!out_name_expr(x, ea, BADADDR))
 		{
 			out_tagon(COLOR_ERROR);
-			OutLong(ea, 16);
+			out_btoa /*		OutLong*/(ea, 16);
 			out_tagoff(COLOR_ERROR);
-			QueueSet(Q_noName, cmd.ea);
+			//QueueSet(Q_noName, insn.ea);
+			remember_problem(PR_NONAME, insn.ea);
 		}
-		if (cmd.indirect)
+		if (insn.indirect)
 			out_symbol(')');
 		break;
 	case o_phrase:
@@ -92,12 +96,12 @@ bool idaapi outop(op_t& x)
 		case riX:
 		case riY:
 			out_symbol('(');
-			out_register(x.phrase == riX ? ph.regNames[rX] : ph.regNames[rY]);
+			out_register(x.phrase == riX ? ph.reg_names[rX] : ph.reg_names[rY]);
 			out_symbol(')');
 			break;
 		case riXinc:
 			out_symbol('(');
-			out_register(ph.regNames[rX]);
+			out_register(ph.reg_names[rX]);
 			out_symbol(')');
 			out_symbol('+');
 			break;
@@ -115,13 +119,13 @@ bool idaapi outop(op_t& x)
 		case rDY:
 			out_dp(x);
 			out_symbol('+');
-			out_register(x.phrase == rDX ? ph.regNames[rX] : ph.regNames[rY]);
+			out_register(x.phrase == rDX ? ph.reg_names[rX] : ph.reg_names[rY]);
 			break;
 		case riDX:
 			out_symbol('(');
 			out_dp(x);
 			out_symbol('+');
-			out_register(ph.regNames[rX]);
+			out_register(ph.reg_names[rX]);
 			out_symbol(')');
 			break;
 		case rDiY:
@@ -129,19 +133,19 @@ bool idaapi outop(op_t& x)
 			out_dp(x);
 			out_symbol(')');
 			out_symbol('+');
-			out_register(ph.regNames[rY]);
+			out_register(ph.reg_names[rY]);
 			break;
 		case rAbsX:
 		case rAbsY:
 			if (!out_name_expr(x, x.addr, BADADDR))
-				OutValue(x, OOF_ADDR | OOFS_NOSIGN | OOFW_16);
+				out_value/* OutValue*/(x, OOF_ADDR | OOFS_NOSIGN | OOFW_16);
 			out_symbol('+');
-			out_register(x.phrase == rAbsX ? ph.regNames[rX] : ph.regNames[rY]);
+			out_register(x.phrase == rAbsX ? ph.reg_names[rX] : ph.reg_names[rY]);
 			break;
 		case rAbsXi:
 			out_symbol('(');
 			if (!out_name_expr(x, x.addr, BADADDR))
-				OutValue(x, OOF_ADDR | OOFS_NOSIGN | OOFW_16);
+				out_value/* OutValue*/(x, OOF_ADDR | OOFS_NOSIGN | OOFW_16);
 			out_symbol('+');
 			out_register(ph.reg_names[rX]);
 			out_symbol(')');
@@ -151,15 +155,15 @@ bool idaapi outop(op_t& x)
 			// fall through
 		case rDbit:
 			if (!out_name_expr(x, x.addr, BADADDR))
-				OutValue(x, OOF_ADDR | OOFS_NOSIGN | OOFW_16);
+				out_value/* OutValue*/(x, OOF_ADDR | OOFS_NOSIGN | OOFW_16);
 			out_symbol('.');
-			OutValue(x, 0);
+			out_value/* OutValue*/(x, 0);
 			break;
 		case rTCall:
-			OutLong(x.value, 10);
+			out_btoa /*OutLong*/(x.value, 10);
 			break;
 		case rPCall:
-			OutLong(x.value, 16);
+			out_btoa /*OutLong*/(x.value, 16);
 			break;
 		default:
 			goto err;
@@ -169,7 +173,7 @@ bool idaapi outop(op_t& x)
 		return 0;
 	default:
 	err:
-		warning("out: %a: bad optype %d", cmd.ea, x.type);
+		warning("out: %a: bad optype %d", insn.ea, x.type);
 		break;
 	}
 	return 1;
@@ -182,19 +186,20 @@ inline bool forced_print(ea_t ea, int /*reg*/)
 }
 
 //----------------------------------------------------------------------
-void idaapi assumes(ea_t ea)
+//void idaapi assumes(ea_t ea)
+void    idaapi assumes(outctx_t& ctx, ea_t ea)
 {
 	char buf[MAXSTR];
 	char* ptr = buf;
 	char* end = buf + sizeof(buf);
-	for (int reg = ph.regFirstSreg; reg <= ph.regLastSreg; reg++)
+	for (int reg = ph.reg_first_sreg/*  .regFirstSreg*/; reg <= ph.reg_last_sreg /* .regLastSreg*/; reg++)
 	{
 		if (reg == rCs || reg == rDs)
 			continue;
-		segreg_area_t srarea;
-		get_srarea2(&srarea, ea, reg);
-		segreg_area_t prev;
-		bool prev_exists = get_srarea2(&prev, ea - 1, reg);
+		sreg_range_t /*segreg_area_t*/ srarea;
+		get_sreg_range /*get_srarea2*/(&srarea, ea, reg);
+		sreg_range_t /*segreg_area_t*/ prev;
+		bool prev_exists = get_sreg_range /*get_srarea2*/(&prev, ea - 1, reg);
 		// if 'prev' does not exist, force to print because we are at
 		// the beginning of the segment
 		sel_t curval = srarea.val;
@@ -203,23 +208,25 @@ void idaapi assumes(ea_t ea)
 		{
 			if (reg == rFp)
 			{
-				gen_cmt_line("P=%d", curval);
+				ctx.gen_cmt_line("P=%d", curval);
 			}
 			else
 			{
 				if (ptr != buf)
 					APPCHAR(ptr, end, ' ');
-				ptr += qsnprintf(ptr, end - ptr, "%s=%a", ph.regNames[reg], curval);
+				ptr += qsnprintf(ptr, end - ptr, "%s=%a", ph.reg_names[reg], curval);
 			}
 		}
 	}
 	if (ptr != buf)
-		gen_cmt_line("%s", buf);
+		ctx.gen_cmt_line("%s", buf);
 }
 
 //----------------------------------------------------------------------
-void idaapi out(void)
+//void idaapi out(void)
+void out_spc700_t::out_insn(void)
 {
+#ifdef GOMWING
 	char buf[MAXSTR];
 
 	init_output_buffer(buf, sizeof(buf));
@@ -232,87 +239,99 @@ void idaapi out(void)
 	}
 
 	OutMnem();
+#else
+	out_mnemonic();
+#endif
 	out_one_operand(0);
-	if (cmd.Op2.type != o_void)
+	if (insn.Op2.type != o_void)
 	{
 		out_symbol(',');
-		OutChar(' ');
+		out_char(' ');
 		out_one_operand(1);
 	}
-
+#ifdef GOMWING
 	if (isVoid(cmd.ea, uFlag, 0))
 		OutImmChar(cmd.Op1);
 
 	term_output_buffer();
 	gl_comm = 1;
 	MakeLine(buf);
+#else
+	out_immchar_cmts();
+	flush_outbuf();
+#endif
 }
 
 //--------------------------------------------------------------------------
-void idaapi header(void)
+//void idaapi header(void)
+void idaapi header(outctx_t& ctx)
 {
-	gen_cmt_line("%s Processor:        %s", ash.cmnt, inf.procName);
-	gen_cmt_line("%s Target assembler: %s", ash.cmnt, ash.name);
+	ctx.gen_cmt_line("%s Processor:        %s", ash.cmnt, inf.procname);
+	ctx.gen_cmt_line("%s Target assembler: %s", ash.cmnt, ash.name);
 	if (ash.header != NULL)
 		for (const char* const* ptr = ash.header; *ptr != NULL; ptr++)
-			MakeLine(*ptr, 0);
+			ctx.flush_buf/*  MakeLine*/(*ptr, 0);
 }
 
 //--------------------------------------------------------------------------
-void idaapi segstart(ea_t ea)
+//void idaapi segstart(ea_t ea)
+void idaapi segstart(outctx_t& ctx, segment_t* seg)
 {
-	segment_t* Sarea = getseg(ea);
-	char name[MAXNAMELEN];
-	get_segm_name(Sarea, name, sizeof(name));
+	//segment_t* Sarea = getseg(ea);
+	ea_t ea = ctx.insn_ea;
+	qstring name; //char name[MAXNAMELEN];
+	get_visible_segm_name(&name, seg); //get_segm_name(Sarea, name, sizeof(name));
 	if (ash.uflag & UAS_SECT)
 	{
-		printf_line(0, COLSTR("%s: .section", SCOLOR_ASMDIR), name);
+		ctx.gen_printf/*printf_line*/(0, COLSTR("%s: .section", SCOLOR_ASMDIR), name);
 	}
 	else
 	{
-		printf_line(inf.indent, COLSTR("%s.segment %s", SCOLOR_ASMDIR),
+		ctx.gen_printf/*printf_line*/(inf.indent, COLSTR("%s.segment %s", SCOLOR_ASMDIR),
 			(ash.uflag & UAS_NOSEG) ? ash.cmnt : "",
 			name);
 		if (ash.uflag & UAS_SELSG)
-			MakeLine(name, inf.indent);
+			ctx.flush_buf/*  MakeLine*/(name.c_str(), inf.indent);
 		if (ash.uflag & UAS_CDSEG)
-			MakeLine(COLSTR("CSEG", SCOLOR_ASMDIR), inf.indent);  // XSEG - eXternal memory
+			ctx.flush_buf/*  MakeLine*/(COLSTR("CSEG", SCOLOR_ASMDIR), inf.indent);  // XSEG - eXternal memory
 	}
-	if (inf.s_org)
+	//if (inf.s_org)
+	if ((inf.outflags & OFLG_GEN_ORG) != 0)
 	{
-		ea_t org = ea - get_segm_base(Sarea);
+		//ea_t org = ea - get_segm_base(Sarea);
+		ea_t org = ea - get_segm_base(seg);
 		if (org != 0)
 		{
 			char buf[MAX_NUMBUF];
 			btoa(buf, sizeof(buf), org);
-			printf_line(inf.indent, COLSTR("%s %s", SCOLOR_ASMDIR), ash.origin, buf);
+			ctx.gen_printf/*printf_line*/(inf.indent, COLSTR("%s %s", SCOLOR_ASMDIR), ash.origin, buf);
 		}
 	}
 }
 
 //--------------------------------------------------------------------------
-void idaapi footer(void)
+//void idaapi footer(void)
+void idaapi footer(outctx_t& ctx)
 {
 	char buf[MAXSTR];
 	if (ash.end != NULL)
 	{
-		MakeNull();
-		outctx_base_t::gen_empty_line
+		ctx.gen_empty_line(); //MakeNull();
 		char* ptr = buf;
 		char* end = buf + sizeof(buf);
 		APPEND(ptr, end, ash.end);
 		qstring name;
-		if (get_colored_name(&name, inf.beginEA) > 0)
+		if (get_colored_name(&name, inf.start_ea /*beginEA*/) > 0)
 		{
 			if (ash.uflag & UAS_NOENS)
 				APPEND(ptr, end, ash.cmnt);
 			APPCHAR(ptr, end, ' ');
 			APPEND(ptr, end, name.begin());
 		}
-		MakeLine(buf, inf.indent);
+		ctx.flush_buf/*  MakeLine*/(buf, inf.indent);
 	}
 	else
 	{
-		gen_cmt_line("end of file");
+		ctx.gen_cmt_line/*  gen_cmt_line*/("end of file");
 	}
 }
